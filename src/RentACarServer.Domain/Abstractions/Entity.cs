9e550f9c022c53777c86369e4c87fb2f3b39ce2c
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-07-14 17:44:05 +0300

    Add project files.

---
+++ src/RentACarServer.Domain/Abstractions/Entity.cs
@@ -0,0 +1,19 @@
+﻿namespace RentACarServer.Domain.Abstractions;
+public abstract class Entity
+{
+    protected Entity()
+    {
+        Id = new IdentityId(Guid.CreateVersion7());
+    }
+    public IdentityId Id { get; set; }
+    public DateTimeOffset CreatedAt { get; set; }
+    public Guid CreatedBy { get; set; }
+    public DateTimeOffset? UpdateAt { get; set; }
+    public Guid? UpdateBy { get; set; }
+    public DateTimeOffset? DeletedAt { get; set; }
+    public Guid? DeletedBy { get; set; }
+    public bool IsDeleted { get; set; }
+
+}
+
+public sealed record IdentityId(Guid Value);
