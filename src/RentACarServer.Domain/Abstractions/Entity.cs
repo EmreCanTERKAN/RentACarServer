3b6c8163d19863950754927b2ca5606939366283
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-07-17 12:23:09 +0300

    Domain katmanı

--- src/RentACarServer.Domain/Abstractions/Entity.cs
+++ src/RentACarServer.Domain/Abstractions/Entity.cs
@@ -4,16 +4,29 @@ public abstract class Entity
     protected Entity()
     {
         Id = new IdentityId(Guid.CreateVersion7());
+        IsActive = true;
     }
-    public IdentityId Id { get; set; }
-    public DateTimeOffset CreatedAt { get; set; }
-    public Guid CreatedBy { get; set; }
-    public DateTimeOffset? UpdateAt { get; set; }
-    public Guid? UpdateBy { get; set; }
-    public DateTimeOffset? DeletedAt { get; set; }
-    public Guid? DeletedBy { get; set; }
-    public bool IsDeleted { get; set; }
-
+    public IdentityId Id { get; private set; }
+    public bool IsActive { get; private set; }
+    public DateTimeOffset CreatedAt { get; private set; }
+    public IdentityId CreatedBy { get; private set; } = default!;
+    public DateTimeOffset? UpdateAt { get; private set; }
+    public IdentityId? UpdateBy { get; private set; }
+    public DateTimeOffset? DeletedAt { get; private set; }
+    public IdentityId? DeletedBy { get; private set; }
+    public bool IsDeleted { get; private set; }
+    public void SetStatus (bool isActive)
+    {
+        IsActive = isActive;
+    }
+    public void Delete()
+    {
+        IsDeleted = true;
+    }
 }
 
-public sealed record IdentityId(Guid Value);
+public sealed record IdentityId(Guid Value)
+{
+    public static implicit operator Guid(IdentityId id) => id.Value;
+    public static implicit operator string(IdentityId id) => id.Value.ToString();
+}
