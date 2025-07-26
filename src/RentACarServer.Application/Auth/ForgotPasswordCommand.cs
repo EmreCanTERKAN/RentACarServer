5887ce0e4077ee6c69d56d1dd1d52f843d3e9b30
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-07-26 15:00:31 +0300

    mail gönderme metodu yazıldı

---
+++ src/RentACarServer.Application/Auth/ForgotPasswordCommand.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using RentACarServer.Domain.Users;
+using TS.MediatR;
+using TS.Result;
+
+namespace RentACarServer.Application.Auth;
+public sealed record ForgotPasswordCommand(
+    string Email) : IRequest<Result<string>>;
+
+public sealed class ForgotPasswordCommandValidator : AbstractValidator<ForgotPasswordCommand>
+{
+    public ForgotPasswordCommandValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email adresi boş olamaz.")
+            .EmailAddress().WithMessage("Geçerli bir mail adresi girin");
+    }
+}
+
+internal sealed class ForgotPasswordCommandHandler(
+    IUserRepository userRepository) : IRequestHandler<ForgotPasswordCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(ForgotPasswordCommand request, CancellationToken cancellationToken)
+    {
+        var user = await userRepository.FirstOrDefaultAsync(x => x.Email.Value == request.Email, cancellationToken);
+
+        if (user is null)
+        {
+            return Result<string>.Failure("Kullanıcı bulunamadı");
+        }
+
+        //şifre sıfırlama maili
+
+        return "Şifre sıfırlama maili gönderildi";
+    }
+}
--- src/RentACarServer.WebApi/Program.cs
+++ src/RentACarServer.WebApi/Program.cs
@@ -27,6 +27,12 @@ builder.Services.AddRateLimiter(cfr =>
         opt.Window = TimeSpan.FromMinutes(1);
         opt.QueueProcessingOrder = System.Threading.RateLimiting.QueueProcessingOrder.OldestFirst;
     });
+    cfr.AddFixedWindowLimiter("forgot-password-fixed", opt =>
+    {
+        opt.PermitLimit = 2;
+        opt.Window = TimeSpan.FromMinutes(3);
+        opt.QueueProcessingOrder = System.Threading.RateLimiting.QueueProcessingOrder.OldestFirst;
+    });
 });
 #endregion
 #region OData
