bd52519fe49900d8c3a8cd674e2d2d567ce896c5
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-08-11 00:06:42 +0300

    Branch get metodu

---
+++ src/RentACarServer.Application/Branches/BranchGetQuery.cs
@@ -0,0 +1,25 @@
+﻿using RentACarServer.Domain.Branches;
+using RentACarServer.Domain.Branchs;
+using TS.MediatR;
+using TS.Result;
+
+namespace RentACarServer.Application.Branches;
+public sealed record BranchGetQuery(
+    Guid Id) : IRequest<Result<Branch>>;
+
+
+internal sealed class BranchQueryHandler(
+    IBranchRepository branchRepository) : IRequestHandler<BranchGetQuery, Result<Branch>>
+{
+    public async Task<Result<Branch>> Handle(BranchGetQuery request, CancellationToken cancellationToken)
+    {
+        var branch = await branchRepository.FirstOrDefaultAsync(i => i.Id == request.Id, cancellationToken);
+
+        if (branch is null)
+        {
+            return Result<Branch>.Failure("Şube bulunamadı");
+        }
+
+        return branch;
+    }
+}
--- src/RentACarServer.Domain/Abstractions/Entity.cs
+++ src/RentACarServer.Domain/Abstractions/Entity.cs
@@ -1,4 +1,8 @@
-﻿namespace RentACarServer.Domain.Abstractions;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using RentACarServer.Domain.Users;
+
+namespace RentACarServer.Domain.Abstractions;
 public abstract class Entity
 {
     protected Entity()
@@ -10,12 +14,14 @@ public abstract class Entity
     public bool IsActive { get; private set; }
     public DateTimeOffset CreatedAt { get; private set; }
     public IdentityId CreatedBy { get; private set; } = default!;
+    public string CreatedFullName => GetCreatedFullName();
     public DateTimeOffset? UpdatedAt { get; private set; }
     public IdentityId? UpdatedBy { get; private set; }
+    public string? UpdatedFullName => GetUpdateFullName();
     public DateTimeOffset? DeletedAt { get; private set; }
     public IdentityId? DeletedBy { get; private set; }
     public bool IsDeleted { get; private set; }
-    public void SetStatus (bool isActive)
+    public void SetStatus(bool isActive)
     {
         IsActive = isActive;
     }
@@ -23,10 +29,56 @@ public abstract class Entity
     {
         IsDeleted = true;
     }
-}
 
-public sealed record IdentityId(Guid Value)
-{
-    public static implicit operator Guid(IdentityId id) => id.Value;
-    public static implicit operator string(IdentityId id) => id.Value.ToString();
+    private string GetCreatedFullName()
+    {
+        HttpContextAccessor httpContextAccessor = new();
+        var httpContext = httpContextAccessor.HttpContext;
+        if (httpContext is null)
+        {
+            return string.Empty;
+        }
+
+        var srv = httpContext.RequestServices;
+        using var scope = srv.CreateScope();
+
+        var userRepository = scope.ServiceProvider.GetService<IUserRepository>()!;
+        if (userRepository is null) return string.Empty;
+
+        var userFullName = userRepository.FirstOrDefault(i => i.Id == CreatedBy).FullName;
+        if (userFullName is null) return string.Empty;
+        return userFullName.Value;
+    }
+
+    private string? GetUpdateFullName()
+    {
+        if (UpdatedBy is not null)
+        {
+            HttpContextAccessor httpContextAccessor = new();
+            var httpContext = httpContextAccessor.HttpContext;
+            if (httpContext is null)
+            {
+                return string.Empty;
+            }
+
+            var srv = httpContext.RequestServices;
+            using var scope = srv.CreateScope();
+
+            var userRepository = scope.ServiceProvider.GetService<IUserRepository>()!;
+            if (userRepository is null) return string.Empty;
+
+            var userFullName = userRepository.FirstOrDefault(i => i.Id == UpdatedBy).FullName;
+            if (userFullName is null) return string.Empty;
+
+            return userFullName.Value;
+        }
+        return null;
+    }
+
+    public sealed record IdentityId(Guid Value)
+    {
+        public static implicit operator Guid(IdentityId id) => id.Value;
+        public static implicit operator string(IdentityId id) => id.Value.ToString();
+    }
+
 }
