1f83a0ffaa233409440d93c24588e2ce77a06389
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-08-10 23:48:56 +0300

    delete işlemi tamamlandı

---
+++ src/RentACarServer.Application/Branches/BranchDeleteCommand.cs
@@ -0,0 +1,30 @@
+﻿using GenericRepository;
+using RentACarServer.Domain.Branchs;
+using TS.MediatR;
+using TS.Result;
+
+namespace RentACarServer.Application.Branches;
+public sealed record BranchDeleteCommand(
+    Guid Id) : IRequest<Result<string>>;
+
+
+internal sealed class BranchDeleteCommandHandler(
+    IBranchRepository branchRepository,
+    IUnitOfWork unitOfWork) : IRequestHandler<BranchDeleteCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(BranchDeleteCommand request, CancellationToken cancellationToken)
+    {
+        var branch = await branchRepository.FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+
+        if (branch is null)
+        {
+            return Result<string>.Failure("Şube bulunamadı");
+        }
+
+        branch.Delete();
+
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return "Şube başarıyla silindi";
+    }
+}
