b199deae7f85288fbd902329bdaf85cc23c312d9
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-08-12 15:30:19 +0300

    GetAll query yapımı joinleme

---
+++ src/RentACarServer.Domain/Abstractions/EntityDto.cs
@@ -0,0 +1,14 @@
+﻿using static RentACarServer.Domain.Abstractions.Entity;
+
+namespace RentACarServer.Domain.Abstractions;
+public abstract class EntityDto
+{
+    public IdentityId Id { get; set; } = default!;
+    public bool IsActive { get; set; }
+    public DateTimeOffset CreatedAt { get; set; }
+    public IdentityId CreatedBy { get; set; } = default!;
+    public string CreatedFullName { get; set; } = default!;
+    public DateTimeOffset? UpdatedAt { get; set; }
+    public IdentityId? UpdatedBy { get; set; }
+    public string? UpdatedFullName { get; set; } 
+}
