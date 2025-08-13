28b97dcd6b88be969ba6a6673dd03161405a4546
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-08-13 12:38:30 +0300

    GetAll için MapTo metodu yazalım

---
+++ src/RentACarServer.Application/Branches/BranchDto.cs
@@ -0,0 +1,42 @@
+﻿using RentACarServer.Domain.Abstractions;
+using RentACarServer.Domain.Branches;
+using RentACarServer.Domain.Branchs.ValueObjects;
+using RentACarServer.Domain.Users;
+
+namespace RentACarServer.Application.Branches;
+
+public sealed class BranchDto : EntityDto
+{
+    public string Name { get; set; } = default!;
+    public Address Address { get; set; } = default!;
+}
+
+public static class BranchExtensions
+{
+    public static IQueryable<BranchDto> MapTo(this IQueryable<Branch> entity, IQueryable<User> users)
+    {
+        var response = entity.Join(users, m => m.CreatedBy, m => m.Id, (b, user) => new { b = b, user = user })
+            .GroupJoin(users, m => m.b.UpdatedBy, m => m.Id, (entity, user) => new { entity = entity, user = user })
+            .SelectMany(s => s.user.DefaultIfEmpty(),
+            (x, user) => new
+            {
+                entity = x.entity,
+                updatedUser = user
+            })
+            .Select(s => new BranchDto
+            {
+                Id = s.entity.b.Id,
+                Name = s.entity.b.Name.Value,
+                Address = s.entity.b.Address,
+                CreatedAt = s.entity.b.CreatedAt,
+                CreatedBy = s.entity.b.CreatedBy,
+                IsActive = s.entity.b.IsActive,
+                UpdatedAt = s.entity.b.UpdatedAt,
+                UpdatedBy = s.entity.b.UpdatedBy == null ? null : s.entity.b.UpdatedBy.Value,
+                CreatedFullName = s.entity.user.FullName.Value,
+                UpdatedFullName = s.updatedUser == null ? null : s.updatedUser.FullName.Value
+            });
+
+        return response;
+    }
+}
