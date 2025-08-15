333068ed49febe0800d28a2711d6640ed43000a5
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-08-15 17:13:47 +0300

    role delete tasarlandı

---
+++ src/RentACarServer.Application/Roles/RoleDeleteCommand.cs
@@ -0,0 +1,30 @@
+﻿using GenericRepository;
+using RentACarServer.Domain.Role;
+using TS.MediatR;
+using TS.Result;
+
+namespace RentACarServer.Application.Roles;
+public sealed record RoleDeleteCommand(
+    Guid Id) : IRequest<Result<string>>;
+
+internal sealed class RoleDeleteCommandHandler(
+    IRoleRepository roleRepository,
+    IUnitOfWork unitOfWork) : IRequestHandler<RoleDeleteCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(RoleDeleteCommand request, CancellationToken cancellationToken)
+    {
+        var role = await roleRepository.FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+
+        if (role is null)
+        {
+            return Result<string>.Failure("Bu id'ye ait rol bulunamadı");
+        }
+
+        role.Delete();
+
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return "Rol başarıyla silindi";
+
+    }
+}
