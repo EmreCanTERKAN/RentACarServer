f61deeb81a8db1ad82727adbf11f9f03435bbf60
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-08-17 00:22:49 +0300

    RoleGetAllQuery metodu tanımlandı

---
+++ src/RentACarServer.Application/Roles/RoleGetAllQuery.cs
@@ -0,0 +1,16 @@
+﻿using RentACarServer.Domain.Role;
+using TS.MediatR;
+
+namespace RentACarServer.Application.Roles;
+public sealed record RoleGetAllQuery() : IRequest<IQueryable<RoleDto>>;
+
+internal sealed class RoleGetAllQueryHandler(
+    IRoleRepository roleRepository) : IRequestHandler<RoleGetAllQuery, IQueryable<RoleDto>>
+{
+    public Task<IQueryable<RoleDto>> Handle(RoleGetAllQuery request, CancellationToken cancellationToken)
+    {
+        var roles = roleRepository.GetAllWithAudit().MapTo();
+
+        return Task.FromResult(roles);
+    }
+}
