b1d6e7501ca5d57bfac481a1e428a5bac56d8df6
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-08-18 16:55:25 +0300

    Role permission ataması için metot değişikliği

--- src/RentACarServer.Application/Roles/RoleDto.cs
+++ src/RentACarServer.Application/Roles/RoleDto.cs
@@ -5,6 +5,8 @@ namespace RentACarServer.Application.Roles;
 public sealed class RoleDto : EntityDto
 {
     public string Name { get; set; } = default!;
+    public int PermissionCount { get; set; }
+    public List<string> Permissions { get; set; } = default!;
 }
 
 public static class RoleExtensions
@@ -15,6 +17,26 @@ public static class RoleExtensions
         {
             Id = s.Entity.Id,
             Name = s.Entity.Name.Value,
+            PermissionCount = s.Entity.Permissions.Count,
+            CreatedAt = s.Entity.CreatedAt,
+            CreatedBy = s.Entity.CreatedBy,
+            CreatedFullName = s.CreatedUser.FullName.Value,
+            UpdatedAt = s.Entity.UpdatedAt,
+            UpdatedBy = s.Entity.UpdatedBy == null ? null : s.Entity.UpdatedBy.Value,
+            UpdatedFullName = s.UpdatedUser == null ? null : s.UpdatedUser.FullName.Value,
+            IsActive = s.Entity.IsActive
+        });
+        return res;
+    }
+
+    public static IQueryable<RoleDto> MapToGet(this IQueryable<EntityWithAuditDto<Role>> entities)
+    {
+        var res = entities.Select(s => new RoleDto
+        {
+            Id = s.Entity.Id,
+            Name = s.Entity.Name.Value,
+            PermissionCount = s.Entity.Permissions.Count,
+            Permissions = s.Entity.Permissions.Select(p => p.Value).ToList(),
             CreatedAt = s.Entity.CreatedAt,
             CreatedBy = s.Entity.CreatedBy,
             CreatedFullName = s.CreatedUser.FullName.Value,
--- src/RentACarServer.Application/Roles/RoleGetQuery.cs
+++ src/RentACarServer.Application/Roles/RoleGetQuery.cs
@@ -17,7 +17,7 @@ internal sealed class RoleGetQueryHandler(
     {
         var role = await roleRepository
             .GetAllWithAudit()
-            .MapTo()
+            .MapToGet()
             .Where(x => x.Id == request.Id)
             .FirstOrDefaultAsync(cancellationToken);
 
