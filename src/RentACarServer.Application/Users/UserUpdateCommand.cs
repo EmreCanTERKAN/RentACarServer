ff6b78b446a257d5a87152186045bc7f3f13c23b
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-08-26 10:18:57 +0300

    Update metodu tamamlandı

---
+++ src/RentACarServer.Application/Users/UserUpdateCommand.cs
@@ -0,0 +1,82 @@
+﻿using FluentValidation;
+using GenericRepository;
+using RentACarServer.Application.Service;
+using RentACarServer.Domain.Abstractions;
+using RentACarServer.Domain.Users;
+using RentACarServer.Domain.Users.ValueObjects;
+using TS.MediatR;
+using TS.Result;
+
+public sealed record UserUpdateCommand(
+    Guid Id,
+    string FirstName,
+    string LastName,
+    string Email,
+    string UserName,
+    Guid? BranchId,
+    Guid RoleId) : IRequest<Result<string>>;
+
+public sealed class UserUpdateCommandValidator : AbstractValidator<UserUpdateCommand>
+{
+    public UserUpdateCommandValidator()
+    {
+        RuleFor(p => p.FirstName).NotEmpty().WithMessage("Geçerli bir ad girin");
+        RuleFor(p => p.LastName).NotEmpty().WithMessage("Geçerli bir soyad girin");
+        RuleFor(p => p.UserName).NotEmpty().WithMessage("Geçerli bir kullanıcı adı girin");
+        RuleFor(p => p.Email)
+            .NotEmpty().WithMessage("Geçerli bir mail adresi girin")
+            .EmailAddress().WithMessage("Geçerli bir mail adresi girin");
+    }
+}
+
+internal sealed class UserUpdateCommandHandler(
+    IUserRepository userRepository,
+    IClaimContext claimContext,
+    IUnitOfWork unitOfWork) : IRequestHandler<UserUpdateCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(UserUpdateCommand request, CancellationToken cancellationToken)
+    {
+
+        var user = await userRepository.FirstOrDefaultAsync(p => p.Id == request.Id, cancellationToken);
+
+        if (user is null)
+        {
+            return Result<string>.Failure("Kullanıcı bulunamadı");
+        }
+
+        if (request.Email != user.Email.Value)
+        {
+            var emailExists = await userRepository.AnyAsync(p => p.Email.Value == request.Email, cancellationToken);
+            if (emailExists)
+            {
+                return Result<string>.Failure("Bu mail adresi daha önce kullanılmış");
+            }
+        }
+
+        if (request.UserName != user.UserName.Value)
+        {
+            var userNameExists = await userRepository.AnyAsync(p => p.UserName.Value == request.UserName, cancellationToken);
+            if (userNameExists)
+            {
+                return Result<string>.Failure("Bu kullanıcı adı daha önce kullanılmış");
+            }
+        }
+
+
+        var branchId = claimContext.GetBranchId();
+        if (request.BranchId is not null)
+        {
+            branchId = request.BranchId.Value;
+        }
+        user.SetFirstName(new FirstName(request.FirstName));
+        user.SetLastName(new LastName(request.LastName));
+        user.SetEmail(new Email(request.Email));
+        user.SetUserName(new UserName(request.UserName));
+        user.SetBranchId(new IdentityId(branchId));
+        user.SetRoleId(new IdentityId(request.RoleId));
+        userRepository.Update(user);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return "Kullanıcı başarıyla güncellendi";
+    }
+}
