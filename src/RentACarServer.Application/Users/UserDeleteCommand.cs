fbee6803197cadf594e6814acf812571a3d149ed
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-08-26 11:28:07 +0300

    Delete Metodu tamamlandı

---
+++ src/RentACarServer.Application/Users/UserDeleteCommand.cs
@@ -0,0 +1,27 @@
+﻿using GenericRepository;
+using RentACarServer.Domain.Users;
+using TS.MediatR;
+using TS.Result;
+
+namespace RentACarServer.Application.Users;
+public sealed record UserDeleteCommand(
+    Guid Id) :  IRequest<Result<string>>;
+
+internal sealed class UserDeleteCommandHandler(
+    IUserRepository userRepository,
+    IUnitOfWork unitOfWork) : IRequestHandler<UserDeleteCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(UserDeleteCommand request, CancellationToken cancellationToken)
+    {
+        var user = await userRepository.FirstOrDefaultAsync(p => p.Id == request.Id, cancellationToken);
+
+        if (user is null)
+        {
+            return Result<string>.Failure("Kullanıcı bulunamadı");
+        }
+
+        user.Delete();
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+        return "Kullanıcı başarıyla silindi";
+    }
+}
