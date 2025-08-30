7aed532bd61bb08361467e2ae5800c4054da52af
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-08-30 10:42:32 +0300

    Category Configuration  tanımlandı

---
+++ src/RentACarServer.Infrastructure/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RentACarServer.Domain.Categories;
+
+namespace RentACarServer.Infrastructure.Configurations;
+internal sealed class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.ToTable("Categories");
+        builder.HasKey(i => i.Id);
+        builder.OwnsOne(i => i.Name);
+    }
+}
